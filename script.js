c84d7664cd05165196dcb352bbbce471d1410412
Denise Vivar <denise@iurco.com>
2026-06-21 15:50:31 -0300

    Cambiar mensaje de error por alert y eliminar required

--- script.js
+++ script.js
@@ -1,53 +1,36 @@
 document.getElementById("formulario").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    document.getElementById("errorNombre").textContent = "";
-    document.getElementById("errorEmail").textContent = "";
-    document.getElementById("errorEdad").textContent = "";
-    document.getElementById("errorPais").textContent = "";
-    document.getElementById("errorTerminos").textContent = "";
-
     const nombre = document.getElementById("nombre").value.trim();
     const email = document.getElementById("email").value.trim();
     const edad = Number(document.getElementById("edad").value);
     const pais = document.getElementById("pais").value;
     const terminos = document.getElementById("terminos").checked;
 
-    let formularioValido = true;
-
-    if (nombre.length < 5) {
-        document.getElementById("errorNombre").textContent =
-            "El nombre debe tener al menos 5 caracteres.";
-        formularioValido = false;
+    if (nombre.length < 5){
+        alert("El nombre debe tener al menos 5 caracteres");
+        return;
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (!emailRegex.test(email)) {
-        document.getElementById("errorEmail").textContent =
-            "Ingresa un email válido.";
-        formularioValido = false;
+        alert("Ingresa un email válido.");
+        return;
     }
 
     if (edad < 18 || edad > 60 || isNaN(edad)) {
-        document.getElementById("errorEdad").textContent =
-            "La edad debe estar entre 18 y 60 años.";
-        formularioValido = false;
+        alert("La edad debe estar entre 18 y 60 años.");
+        return;
     }
 
     if (pais === "") {
-        document.getElementById("errorPais").textContent =
-            "Selecciona un país.";
-        formularioValido = false;
+        alert("Selecciona un país.");
+        return;
     }
 
     if (!terminos) {
-        document.getElementById("errorTerminos").textContent =
-            "Debes aceptar los términos y condiciones.";
-        formularioValido = false;
-    }
-
-    if (!formularioValido) {
+        alert("Debes aceptar los términos y condiciones.");
         return;
     }
 
